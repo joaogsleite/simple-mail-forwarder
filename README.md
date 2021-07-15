9855e51c3ae4292d0d26e5851caaaaadeb57e3df
João Leite <joao.leite@isobar.com>
2021-07-15 12:45:09 +0100

    install script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Mail Forwarder
+
+Systemctl service based on [docker-simple-mail-forwarder](https://github.com/huan/docker-simple-mail-forwarder)
+
+## How to install?
+
+```
+bash <(curl -s https://raw.githubusercontent.com/joaogsleite/simple-mail-forwarder/master/install.sh)
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,35 @@
-sudo cp simple-mail-forwarder /usr/bin/simple-mail-forwarder
+#!/usr/bin/env bash
+
+BASE_URL="https://raw.githubusercontent.com/joaogsleite/simple-mail-forwarder/master/"
+
+sudo apt update -y
+
+# docker
+sudo apt install -y docker.io
+sudo systemctl enable docker
+
+# wget
+sudo apt install -y wget
+
+# docker-compose
+sudo apt install -y jq
+VERSION=\$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+OS=\$(uname -s)
+ARCH=\$(uname -m)
+sudo curl -L "https://github.com/docker/compose/releases/download/\$VERSION/docker-compose-\$OS-\$ARCH" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# simple-mail-forwarder script
+sudo wget -O /usr/bin/simple-mail-forwarder $BASE_URL/simple-mail-forwarder
 sudo chmod +x /usr/bin/simple-mail-forwarder
-sudo cp simple-mail-forwarder.service /etc/systemd/system/simple-mail-forwarder.service
+
+# simple-mail-forwader configs
+sudo mkdir -p /etc/simple-mail-forwarder/mail.d/
+echo "@example.com:exampleaccount@gmail.com" > /etc/simple-mail-forwarder/mail.d/example.txt
+
+# simple-mail-forwarder service
+sudo wget -O /etc/systemd/system/simple-mail-forwarder.service $BASE_URL/simple-mail-forwarder.service
 sudo systemctl daemon-reload
 sudo systemctl start simple-mail-forwarder
 sudo systemctl enable simple-mail-forwarder
+
